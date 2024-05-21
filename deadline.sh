7f7dc31bc6759b10ad202f77d4ca66edd565022b
punisher-303 <mehafin0@gmail.com>
2024-05-21 12:04:50 +0530

    Add files via upload

---
+++ deadline.sh
@@ -0,0 +1,21 @@
+#!/bin/bash/
+
+termux-setup-storage
+apt update && apt upgrade -y
+apt install nmap -y
+apt install nano -y
+apt install python -y
+apt install wget -y
+apt install nodejs -y
+apt install curl -y
+apt install ruby -y
+apt install openssh -y
+pkg install python -y
+pkg install python3 -y
+pkg install git -y
+pkg install wget -y
+pkg install curl -y
+pkg install python2 -y
+apt install python -y 
+pkg install gh -y
+
